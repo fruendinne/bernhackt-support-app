6e1fd203e2139197dc2da7127f59e1950df0e748
jessicachrist <jessica.christ@dreipol.ch>
2020-09-05 21:58:18 +0200

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,98 @@
 <template>
     <v-app>
         <v-main>
-            <v-container>
-                <v-row>
-                    <v-col>
-                        <h2>Verbindungsüberprüfung</h2>
-                        <p>Lassen Sie ihre Verbindung auf mögliche Störungen überprüfen. Wenn Sie das nicht möchten,
-                            können Sie die Verbindungsprüfung überspringen.</p>
-                        <h3>Geben Sie Ihre Kundennummer ein</h3>
-                        <v-text-field
-                                color="primary"
-                                label="Kundennummer">
-                        </v-text-field>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col class="d-flex justify-end">
-                        <v-btn medium text="true" color="primary">Überspringen</v-btn>
-                        <v-btn medium color="accent">Weiter</v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
+            <v-container class="fill-height">
+                <v-col class="align-center">
+                    <v-row no-gutters>
+                        <v-col>
 
+                            <div id="app">
+                                <v-app id="inspire">
+                                    <v-stepper v-model="e6" vertical>
+                                        <v-stepper-step :complete="e6 > 1" step="1">
+                                            Wo liegt das Problem?
+                                        </v-stepper-step>
+
+                                        <v-stepper-content step="1">
+                                            <v-card class="mb-12 stepper--card">
+                                                <OnboardingCustomerNumber/>
+                                            </v-card>
+                                            <v-col class="d-flex justify-end">
+                                                <v-btn medium text color="primary" @click="e6 = 2">Überspringen
+                                                </v-btn>
+                                                <v-btn medium color="accent" @click="e6 = 2">Weiter</v-btn>
+                                            </v-col>
+                                        </v-stepper-content>
+
+                                        <v-stepper-step :complete="e6 > 2" step="2">Verbindungsüberprüfung
+                                        </v-stepper-step>
+
+                                        <v-stepper-content step="2">
+                                            <v-card class="mb-12 stepper--card" height="300px">
+                                                <OnboardingCustomerNumber/>
+                                            </v-card>
+                                            <v-col class="d-flex justify-end">
+                                                <v-btn medium text color="primary" @click="e6 = 4">Überspringen</v-btn>
+                                                <v-btn medium color="accent" @click="e6 = 3">Weiter</v-btn>
+                                            </v-col>
+                                        </v-stepper-content>
+
+                                        <v-stepper-step :complete="e6 > 3" step="3">Überprüfung ist am Laufen
+                                        </v-stepper-step>
+
+                                        <v-stepper-content step="3">
+                                            <v-card class="mb-12 stepper--card" height="300px">
+                                                <OnboardingCustomerNumber/>
+                                            </v-card>
+                                            <v-col class="d-flex justify-end">
+                                                <v-btn medium text color="primary" @click="e6 = 2">Zurück</v-btn>
+                                                <v-btn medium color="accent" @click="e6 = 4">Weiter</v-btn>
+                                            </v-col>
+                                        </v-stepper-content>
+
+                                        <v-stepper-step step="4">Schritt 4</v-stepper-step>
+                                        <v-stepper-content step="4">
+                                            <v-card class="mb-12 stepper--card" height="300px">
+                                                <OnboardingCustomerNumber/>
+                                            </v-card>
+                                            <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
+                                            <v-btn text>Cancel</v-btn>
+                                        </v-stepper-content>
+                                    </v-stepper>
+                                </v-app>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script>
+    import OnboardingCustomerNumber from './views/OnboardingCustomerNumber';
+
     export default {
         name: 'App',
+        components: { OnboardingCustomerNumber },
+        data() {
+            return {
+                e6: 1,
+            };
+        },
     };
+
+
 </script>
 
 <style lang="scss">
     @import "sass/variables";
+
+    .stepper--card {
+        height: 300px !important;
+
+        @media screen and(max-width: 800px) {
+            height: 500px !important;
+        }
+    }
 </style>
