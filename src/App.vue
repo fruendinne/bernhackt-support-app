94450b4b9afb9b25201018c28c44ba2839349aa0
hoschiho <pascal.hoschi@gmail.com>
2020-09-05 17:24:16 +0200

    so es paar bildli inedoo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,57 @@
 <template>
   <v-app>
+    <v-app-bar app>
+    </v-app-bar>
     <v-main>
+      <v-container>
+      <v-row no-gutters>
+        <h3>zu welchem thema brauchen sie hilfe?</h3>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div v-ripple class="sexschen-button">
+            <v-avatar class="ma-2" size="96">
+              <img
+                  src="https://quickline.ch/media/r0uc4oce/icon_nav_internet.png"
+              alt="internet">
+            </v-avatar>
+            <span>Internet</span>
+          </div>
+        </v-col>
+        <v-col>
+          <div v-ripple class="sexschen-button">
+            <v-avatar class="ma-2" size="96">
+              <img
+                  src="https://quickline.ch/media/r0uc4oce/icon_nav_internet.png"
+                  alt="internet">
+            </v-avatar>
+            <span>Internet</span>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <div v-ripple class="sexschen-button">
+            <v-avatar class="ma-2" size="96">
+              <img
+                  src="https://quickline.ch/media/r0uc4oce/icon_nav_internet.png"
+                  alt="internet">
+            </v-avatar>
+            <span>Internet</span>
+          </div>
+        </v-col>
+        <v-col>
+          <div v-ripple class="sexschen-button">
+            <v-avatar class="ma-2" size="96">
+              <img
+                  src="https://quickline.ch/media/r0uc4oce/icon_nav_internet.png"
+                  alt="internet">
+            </v-avatar>
+            <span>Internet</span>
+          </div>
+        </v-col>
+      </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -10,3 +61,13 @@ export default {
   name: 'App',
 };
 </script>
+
+<style lang="scss">
+.sexschen-button{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #d40037;
+  font-size: 1.25rem;
+}
+</style>
