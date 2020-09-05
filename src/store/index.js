9559f7996a5d5d71195a4becfade2c2a5311d9ce
Dario Breitenstein <online@imakethings.ch>
2020-09-06 01:55:20 +0200

    add vuex mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { startFlow } from '../services/api'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    flow: null,
   },
   mutations: {
+    setFlow(state, flow) {
+      state.flow = flow
+    }
   },
   actions: {
+    async startFlow({ commit }, userQuery) {
+      commit('setFlow', await startFlow(userQuery))
+    }
   },
   modules: {
   },
