900e2ed287c9fec8acd7d31c4c9b679246a76769
jessicachrist <jessica.christ@dreipol.ch>
2020-09-05 17:25:27 +0200

    Add customer number view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <v-app>
-    <v-main>
-    </v-main>
-  </v-app>
+    <v-app>
+        <v-main>
+            <v-container>
+                <v-row>
+                    <v-col>
+                        <h2>Verbindungsüberprüfung</h2>
+                        <p>Lassen Sie ihre Verbindung auf mögliche Störungen überprüfen. Wenn Sie das nicht möchten,
+                            können Sie die Verbindungsprüfung überspringen.</p>
+                        <h3>Geben Sie Ihre Kundennummer ein</h3>
+                        <v-text-field
+                                color="primary"
+                                label="Kundennummer">
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col class="d-flex justify-end">
+                        <v-btn medium text="true" color="primary">Überspringen</v-btn>
+                        <v-btn medium color="accent">Weiter</v-btn>
+                    </v-col>
+                </v-row>
+            </v-container>
+
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-export default {
-  name: 'App',
-};
+    export default {
+        name: 'App',
+    };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#242424',
+                secondary: '#a5a5a5',
+                accent: '#d40037',
+                error: '#d40037',
+                white: '#ffffff',
+                black: '#000000',
+            },
+        },
+    },
 });
