80f8e61fbea93e817b3e0be36b28e6d11d4e6b96
nightbehaviour <hannalisa.franz@students.fhnw.ch>
2020-09-05 22:29:48 +0200

    bad loading animation - not centered or pretty - but existent x3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <v-app>
     <v-main>
+      <div class="flex-column d-flex align-content-center">
+        <h3>Wir überprüfen nun Ihre
+          Verbindung zu unseren Systemen.
+          Bitte haben Sie einen
+          Moment Geduld.</h3>
+        <v-row align-content="center" justify="center">
+        <v-col cols="6">
+          <v-progress-linear
+                  indeterminate
+                  color="red"
+                  rounded
+                  height="6"
+          ></v-progress-linear>
+        </v-col>
+        </v-row>
+      </div>
     </v-main>
   </v-app>
 </template>
